c86f096ead25b27edc7f1c37c6ae6e5812752f25
LSalazarL <luis.salazar.ing@gmail.com>
2023-07-16 22:24:52 +0200

    Se crea el marco(JFrame) principal

--- src/marco_laminas/Aplicacion_modificar_ventana.java
+++ src/marco_laminas/Aplicacion_modificar_ventana.java
@@ -5,7 +5,8 @@ public class Aplicacion_modificar_ventana {
 	public static void main(String[] args) {
 		
 		Marco_Principal miMarco = new Marco_Principal();
-
+		miMarco.setLocationRelativeTo(null);
+		miMarco.setVisible(true);
 	}
 
 }
--- src/marco_laminas/Marco_Principal.java
+++ src/marco_laminas/Marco_Principal.java
@@ -1,9 +1,15 @@
 package marco_laminas;
 import javax.swing.*;
+import java.awt.*;
 
 public class Marco_Principal extends JFrame{
 	
 	public Marco_Principal() {
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(600,450);
+		setTitle("P01_Probando_JOptionPanel");
+		setIconImage(Toolkit.getDefaultToolkit().getImage("src/img/iconoBall.png"));
 		
 	}
 
