a57dc6360b5d9b756752167dd7942a322364aa9e
LSalazarL <luis.salazar.ing@gmail.com>
2023-07-19 00:00:04 +0200

    Se crea y termina la estructura del programa

--- src/marco_laminas/Lamina_Botones.java
+++ src/marco_laminas/Lamina_Botones.java
@@ -3,8 +3,37 @@ import javax.swing.*;
 
 public class Lamina_Botones extends JPanel{
 
-	public Lamina_Botones() {
+	public Lamina_Botones(String titulo, String[] opciones) {
+		
+		setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), titulo));//se  crea  el borde del JPanel
+		
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+		grupo = new ButtonGroup();
+		
+		for(int i = 0; i < opciones.length; i++) {
+			
+			JRadioButton bot = new JRadioButton(opciones[i]);
+			
+			bot.setActionCommand(opciones[i]);
+			
+			add(bot);//se agrega a la lamina los JRadioButtons
+			grupo.add(bot);//se agrega al grupo para tener solo uno seleccionado
+			bot.setSelected(i==0);//para que aparesca el primer elemento seleccionado
+			
+		}
 		
 	}
+	
+	public String dameSeleccion() {
+		
+		ButtonModel miBoton = grupo.getSelection();//el objeto miBoton tiene almacenado el radiobutton que este seleccionado
+		String s = miBoton.getActionCommand();//el metodo getActionCommand devuelve el string del objeto component (en este caso el radiobutton)
+	
+		return s;
+		
+	}
+	
+	private ButtonGroup grupo;
 	
 }
--- src/marco_laminas/Marco_Principal.java
+++ src/marco_laminas/Marco_Principal.java
@@ -10,7 +10,50 @@ public class Marco_Principal extends JFrame{
 		setSize(600,450);
 		setTitle("P01_Probando_JOptionPanel");
 		setIconImage(Toolkit.getDefaultToolkit().getImage("src/img/iconoBall.png"));
+		setLayout(new BorderLayout());
+		
+		// JPanel que almacenara las diferentes opciones
+		JPanel lamina_cuadricula = new JPanel();
+		lamina_cuadricula.setLayout(new GridLayout(2,3));
+		
+		// Se crean los Arrays con las diferentes opciones para los JRadioButton
+		String cajaN1_tipo[] = {"Mensaje","Confirmar","Opción","Entrada"};
+		String cajaN2_tipoMensaje[] = {"ERROR_MESSAGE","INFORMATION_MESSAGE","WARNING_MESSAGE","QUESTION_MESSAGE","PLAIN_MESSAGE"};
+		String cajaN3_mensaje[] = {"Cadena","Icono","Componente","Otros","Object[]"};
+		String cajaN4_confirmar[] = {"DEFAULT_OPTION","YES_NO_OPTION","YES_NO_CANCEL_OPTION","OK_CANCEL_OPTION"};
+		String cajaN5_opcion[] = {"String[]","Icon[]","Object[]"};
+		String cajaN6_entrada[] = {"Campo de Texto","Combo"};
+		
+		// Creando las instancias del JPanel (Lamina_Botones)
+		lamina_tipo = new Lamina_Botones("Tipo", cajaN1_tipo);
+		lamina_tipo_mensaje = new Lamina_Botones("Tipo de Mensaje", cajaN2_tipoMensaje);
+		lamina_mensaje = new Lamina_Botones("Mensaje", cajaN3_mensaje);
+		lamina_tipo_opcion = new Lamina_Botones("Confirmar", cajaN4_confirmar);
+		lamina_opciones = new Lamina_Botones("Opción", cajaN5_opcion);
+		lamina_entrada = new Lamina_Botones("Entrada", cajaN6_entrada);
+		
+		// Agregado los JPanel(Lamina_Botones), al JPanel(lamina_cuadricula) con disposición GridLayout		
+		lamina_cuadricula.add(lamina_tipo);
+		lamina_cuadricula.add(lamina_tipo_mensaje);
+		lamina_cuadricula.add(lamina_mensaje);
+		lamina_cuadricula.add(lamina_tipo_opcion);
+		lamina_cuadricula.add(lamina_opciones);
+		lamina_cuadricula.add(lamina_entrada);
+		
+		// JPanel para el boton
+		JPanel lamina_boton = new JPanel();
+		JButton btnMostrar = new JButton("Mostrar");
+		lamina_boton.add(btnMostrar);
+		
+		// Agregando los JPanel al JFrame principal
+		add(lamina_cuadricula, BorderLayout.CENTER);
+		add(lamina_boton, BorderLayout.SOUTH);
 		
 	}
+	
+	private Lamina_Botones lamina_tipo, lamina_tipo_mensaje, lamina_mensaje, 
+						   lamina_tipo_opcion, lamina_opciones, lamina_entrada;
+	
+	
 
 }
